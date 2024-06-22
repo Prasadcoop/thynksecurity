1fb928c1474db14866bc15a8664ccde025eefe07
PRASAD SANGALE <86963235+Prasadcoop@users.noreply.github.com>
2024-06-22 09:54:45 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 //require __DIR__ . "/../public/index.php";
 
-require __DIR__ . "/../vender/autoload.php";
+// require __DIR__ . "/../vender/autoload.php";
 $app = require_once __DIR__.'/../boostrap/app.php';
 
 $kernel = $app->make(Kernel::class);
